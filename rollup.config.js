1fd9a71f6bd44c9c45b159ff4f41317c0e8babe5
Marisa DeMeglio <marisa.demeglio@gmail.com>
2019-12-20 17:44:23 -0800

    Revert "Add minified version"
    This reverts commit c2bf1a3f8d508a79a33410306a3a1227b270365a.

--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    input: './src/manifest.js',
-    output: {
-        file: './build/pubmanifest-parse.min.js',
-        format: 'esm',
-        name: 'pubmanifest-parse'
-    }
-}
--- src/manifest.js
+++ src/manifest.js
@@ -26,7 +26,7 @@ class Manifest {
         };
         this.readingOrderIndex = 0;
         this.toc = false;
-        this.version = "0.1.1";
+        this.version = "0.1.0";
     }
     
     setSupportedProfiles(supportedProfiles) {
